40c8e4b10b6a5bea6202ca89e70097898b5b3978
Eliza Pant <elizapant.github@gmail.com>
2025-01-21 16:55:59 +0545

    awdwd

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from "react";
 import Navbar from '../components/Navbar';
 import PhysiotherapyServices from '../components/PhysiotherapyServices';
 import Footer from '../components/Footer';
@@ -15,6 +15,10 @@ import DoctorsSlider from '../components/DoctorsSlider';
 
 
 const Home = () => {
+   useEffect(() => {
+      // Scroll to the top when the page loads
+      window.scrollTo(0, 0);
+    }, []);
   return (
     <div>
       {/* Navbar is fixed, so we add a wrapper with padding */}
